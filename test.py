0498e4bb713dc4866fecbe69ae0234b1fb2c5042
motwakil <mutwakil.abdallah@gmail.com>
2023-02-03 08:47:12 +0300

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+import sympy as sym
+import numpy as np
+import scipy.integrate as integrate
+
+
+
+f = lambda x:x**3 - 1
+
+F= abs(integrate.quad(f, -1, 0)[0]) + integrate.quad(f, 0, 1)[0]
+print(F)
+print(f)
